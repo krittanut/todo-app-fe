25588b18c6a8133373002b9a1bd7354f64f65074
Krittanut Sangsri <sangsri.krittanut@gmail.com>
2024-08-23 23:36:36 +0700

    first init commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,14 @@
+export default function Login() {
+
+    return (
+        <div className="bg-green-200 min-h-screen flex justify-center">
+
+            <div className="flex w-2/6 bg-red-500 min-h-screen">
+                <div>
+                    
+                </div>
+            </div>
+
+        </div>
+    )
+};
